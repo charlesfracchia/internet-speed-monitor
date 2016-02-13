f4f034b2e86b618a10b2c1684135dba936d6b26a
Charles Fracchia <charlesfracchia@gmail.com>
2016-02-13 02:55:30 -0500

    Major commit adding dependencies and server code and graph with correct legend

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var fs = require('fs');
+var exphbs = require('express-handlebars');
+
+var app = express();
+app.use(express.static(__dirname + '/server'));
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.get('/', function (req, res) {
+  fs.readFile('bandwidthLog.txt', 'utf8', function (err,data) {
+    if (err) { res.send("Error reading the file: "+err); }
+    data = data.replaceAll("\n","---");
+    console.log(">>> Read "+data.length+" data points");
+    var context = { m : data };
+    res.render('history',context);
+  });
+});
+
+app.listen(12345, function () {
+  console.log('Example app listening on port 12345!');
+});
+
+String.prototype.replaceAll = function(search, replacement) {
+  var target = this;
+  return target.replace(new RegExp(search, 'g'), replacement);
+};
