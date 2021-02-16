2a7e37d25e6ca528a6653a536ecac1ecd0626511
Charles Fracchia <charlesfracchia@gmail.com>
2021-02-15 21:54:36 -0500

    Updated code to use new CSV structure straight from CLI and use Async and Promises

---
+++ read_speed_data.js
@@ -0,0 +1,61 @@
+const createCsvStringifier = require('csv-writer').createObjectCsvStringifier;
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const inputFile = 'internet_log.csv';
+
+const inputCSVJSON = [];
+
+const csvStringifier = createCsvStringifier({
+  header: [
+      {id: 'Timestamp', title: 'Time'},
+      {id: 'Ping', title: 'Ping'},
+      {id: 'Download', title: 'Download'},
+      {id: 'Upload', title: 'Upload'},
+  ]
+});
+
+function parseCSV(inputFile) {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(inputFile)
+    .pipe(csv())
+    .on('data', (data) => inputCSVJSON.push(data))
+    .on('end', async () => {
+      let transformedData = await transformData(inputCSVJSON);
+      data_yep = await outputModifiedCSV(transformedData);
+      resolve(data_yep);
+    });
+  })
+}
+
+function transformData(inputCSVtoTransform){
+  return new Promise((resolve, reject) => {
+    // console.log(inputCSVtoTransform);
+    inputCSVtoTransform.forEach(transformRow);
+    resolve(inputCSVtoTransform);
+  });
+}
+
+function transformRow(row) {
+  return new Promise((resolve, reject) => {
+    row.Timestamp = row.Timestamp.replace(/-/g,"/").replace("T"," ").slice(0,-8);
+    row.Ping = Math.round(row.Ping);
+    row.Download = Math.round(row.Download/1000000);
+    row.Upload = Math.round(row.Upload/1000000);
+    // row.Distance = row.Distance; //for conversion TODO
+    // console.log("---");
+    resolve(row);
+  });
+}
+
+function outputModifiedCSV(modifiedCSVJSON) {
+  return new Promise((resolve, reject) => {
+    resolve(csvStringifier.getHeaderString()+csvStringifier.stringifyRecords(modifiedCSVJSON));
+  });
+}
+
+module.exports = (async function() {
+  const data = await parseCSV(inputFile);
+  // console.log(data);
+  return{ data };
+})();
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var fs = require('fs');
 var exphbs = require('express-handlebars');
 
 var app = express();
@@ -8,20 +7,21 @@ app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
-  fs.readFile('../internet_speed.txt', 'utf8', function (err,data) {
-    if (err) { res.send("Error reading the file: "+err); }
-    data = data.replaceAll("\n","---");
-    console.log(">>> Read "+data.length+" data points");
-    var context = { m : data };
+  (async function(){
+    let speed_data = await require('./read_speed_data');
+    // console.log(speed_data);
+    let data_for_frontend = await speed_data.data.replace(/\n/g, "---");
+
+    let measurements = (data_for_frontend.match(/---/g) || []).length-1;
+    let last_date = speed_data.data.trim().split("\n").slice(-1)[0].split(",")[0];
+    // console.log(last_date);
+    console.log(">>> Read "+measurements+" data points. Latest data from: "+last_date);
+
+    var context = { m : data_for_frontend };
     res.render('history',context);
-  });
+  })();
 });
 
 app.listen(12345, function () {
   console.log('Example app listening on port 12345!');
 });
-
-String.prototype.replaceAll = function(search, replacement) {
-  var target = this;
-  return target.replace(new RegExp(search, 'g'), replacement);
-};
